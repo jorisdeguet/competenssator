2442f3bbe2399c12a3422bf87579597f9c136718
Joris Deguet <joris.deguet@gmail.com>
2023-12-16 16:19:07 -0500

    First commit generator de SVG

---
+++ competenssator/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import competenssator as cs
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World MOtherfucker!'
+
+@app.route('/competenssator')
+def foobar():
+    file_path = '5N6.yaml'
+    results = cs.yaml_to_svgs(file_path)
+    return results[0]
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
